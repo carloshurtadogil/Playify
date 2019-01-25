727048e8aa8355d7574c59add9870a24f8117030
Carlos Hurtado <carloshurtado219@gmail.com>
2019-01-25 11:00:58 -0800

    Base Project

---
+++ Playify/src/application/Main.java
@@ -0,0 +1,43 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+//import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font; 
+import javafx.scene.text.FontPosture; 
+import javafx.scene.text.FontWeight; 
+import javafx.scene.text.Text; 
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			//BorderPane root = new BorderPane();
+			Scene scene = new Scene(UI(primaryStage),1080,720);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Playify");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	public Group UI (Stage stage) {
+		Text title = new Text();
+		title.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20)); 
+	    title.setX(490); 
+	    title.setY(90);          
+	    title.setText("Playify");
+	    
+	    Group root = new Group(title);
+	    return root;
+	}
+}
